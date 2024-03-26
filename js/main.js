08e40568787771ed88f5b2dae696e8abd0e294c1
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-03-26 12:45:23 +0700

    add: doibanglaixe tracnghiemlaixe

--- js/main.js
+++ js/main.js
@@ -1,54 +1,64 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
 
+  // Initiate the wowjs
+  new WOW().init();
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
-        }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".sticky-top").addClass("shadow-sm").css("top", "0px");
+    } else {
+      $(".sticky-top").removeClass("shadow-sm").css("top", "-100px");
+    }
+  });
 
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        items: 1,
-        dots: true,
-        loop: true,
-    });
-
-    
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    items: 1,
+    dots: true,
+    loop: true,
+  });
 })(jQuery);
 
+function fDownload(fDownload) {
+  // Lấy đường dẫn hoặc thông tin tài liệu dựa trên documentId
+  var documentUrl = "img/tailieu/"; // Thay đổi URL này bằng đường dẫn của tài liệu thực tế
+
+  // Tạo một phần tử <a> tạm thời để tải tài liệu
+  var tempLink = document.createElement("a");
+  tempLink.href = documentUrl;
+  tempLink.setAttribute("download", "laixenguyentrinh_on-luat-600-cau-hoi-luat-gtdb.rar"); // Thay đổi tên-tài-liệu.pdf bằng tên thực tế của tài liệu
+
+  // Tự động kích hoạt sự kiện nhấn nút để tải tài liệu
+  document.body.appendChild(tempLink);
+  tempLink.click();
+
+  // Xóa phần tử <a> sau khi tải hoàn tất
+  document.body.removeChild(tempLink);
+}
